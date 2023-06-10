273e59d43ab469cf1a14c4ccf56129cf84d4a800
RMalanco <rmalanco@rmalanco.com>
2023-06-09 20:27:34 -0600

    Update

--- BlogCore/Areas/Admin/Controllers/ArticulosController.cs
+++ BlogCore/Areas/Admin/Controllers/ArticulosController.cs
@@ -1,13 +1,8 @@
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using BlogCore.AccesoDatos.Data.Repository.IRepository;
 using BlogCore.Data;
 using BlogCore.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace BlogCore.Areas.Admin.Controllers
 {
